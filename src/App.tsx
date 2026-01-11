3d9a15ba28f6e478f4ad5fedc56cfba71624ab27
Jasper Garcia <garciajapxd@gmail.com>
2026-01-11 21:51:13 +0800

    Improve page not found

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,20 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/favorites" element={<Favorites />}></Route>
-            <Route path="*" element={<div>Page not found</div>} />
+            <Route
+              path="*"
+              element={
+                <div style={{ textAlign: "center", marginTop: "2rem" }}>
+                  <h2>Page not found</h2>
+                  <button
+                    className="btn btn-primary mt-2"
+                    onClick={() => window.history.back()}
+                  >
+                    Go Back
+                  </button>
+                </div>
+              }
+            />
           </Routes>
         </div>
       </MovieProvider>
